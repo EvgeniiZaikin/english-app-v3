47c282acfbaa56fd2630f3d8c4a180774f6b5355
en_zaikin <en_zaikin@bingo-boom.com>
2020-12-22 13:51:01 +0300

    Refactoring store.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,10 @@
-import {createStore, AnyAction} from 'redux';
+import {createStore, AnyAction, Reducer} from 'redux';
 import {MakeStore, createWrapper, Context, HYDRATE} from 'next-redux-wrapper';
 import reducers from './reducers';
 
 export type reducersState = ReturnType<typeof reducers>;
 
-const TEST = (state: reducersState, action: AnyAction) => {
+/*const TEST = (state: reducersState, action: AnyAction) => {
     if (action.type === HYDRATE) {
       const nextState = {
         ...state, // use previous state
@@ -14,7 +14,11 @@ const TEST = (state: reducersState, action: AnyAction) => {
     } else {
       return reducers(state, action)
     }
-  }
+  }*/
+
+const TEST: Reducer<reducersState, AnyAction> = 
+    (state: reducersState, action: AnyAction) => 
+    action.type === HYDRATE ? { ...state, ...action.payload } : reducers(state, action);
 
 const makeStore: MakeStore<reducersState> = (context: Context) => createStore(TEST);
 export const wrapper = createWrapper<reducersState>(makeStore, {debug: true});
