6508e2e4c320c645817673da41f0e137e6a1e76e
EVGENII <evgenii@MacBook-Air-EVGENII.local>
2020-12-31 19:52:10 +0300

    Update create reducer.

--- src/store/reducers/create.ts
+++ src/store/reducers/create.ts
@@ -33,7 +33,7 @@ const initialState: IState = {
 const create: Reducer<IState, AnyAction> = (state = initialState, action) => {
     switch (action.type) {
         case HYDRATE:
-            const hydrateState = action.payload.add;
+            const hydrateState = action.payload.create;
             return { ...hydrateState };
         case SET_TYPE:
             return { ...state, type: action.payload };
