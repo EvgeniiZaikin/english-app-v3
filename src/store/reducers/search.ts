0eb592fa01bc3dcc31f27f8b03cc71e55b7d10cf
EVGENII <evgenii@MacBook-Air-EVGENII.local>
2021-01-02 23:28:22 +0300

    Update search reducer.

--- src/store/reducers/search.ts
+++ src/store/reducers/search.ts
@@ -1,6 +1,10 @@
 import { HYDRATE } from 'next-redux-wrapper';
 import { Reducer, AnyAction } from 'redux';
+import { ThunkDispatch } from 'redux-thunk';
 import { action } from '../reducers';
+import { reducersState } from '@store';
+import axios from 'axios';
+import { showGlobalAlert, AlertTypes, delayHideGlobalAlert } from './global-alert';
 
 export const SET_SEARCH_INFO: string = 'SET_SEARCH_INFO';
 
@@ -21,6 +25,9 @@ const search: Reducer<IState, AnyAction> = (state = initialState, action) => {
         case HYDRATE:
             const hydrateState = action.payload.search;
             return { ...hydrateState };
+        case SET_SEARCH_INFO:
+            const { ruValue, enValue, category } = action.payload;
+            return { ...state, ruValue, enValue, category };
         default:
             return { ...state };
     }
@@ -29,3 +36,26 @@ const search: Reducer<IState, AnyAction> = (state = initialState, action) => {
 export default search;
 
 export const setSearchInfo = (info: IState) => action<IState>(SET_SEARCH_INFO, info);
+export const setSearchData = (ruValue: string, enValue: string) => async (dispatch: ThunkDispatch<reducersState, void, AnyAction>) => {
+    try {
+        const { data } = await axios.get(`/api/words/word`, { params: { ruValue, enValue } });
+        const { status, result, error } = data;
+
+        if (status && result.length && !error) {
+            const [ word ] = result;
+            dispatch(setSearchInfo(word));
+        } else {
+            dispatch(setSearchInfo({
+                ruValue: '',
+                enValue: '',
+                category: '',
+            }));
+            throw new Error(`Status search word is false! Error: ${error}`);
+        }
+    } catch (exception: any) {
+        dispatch(showGlobalAlert(AlertTypes.ERROR, 'Can not search word!'));
+        console.log(exception);
+        
+        delayHideGlobalAlert(dispatch, 1500);
+    }
+};
