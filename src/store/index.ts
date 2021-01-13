e83fc3b2ab9d3c0691461bc4fc67829b991d3edb
EVGENII <evgenii@gericom.darnet.ru>
2021-01-14 00:26:16 +0300

    Remove test data.

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,23 +5,5 @@ import reducers from './reducers';
 
 export type reducersState = ReturnType<typeof reducers>;
 
-/*const TEST = (state: reducersState, action: AnyAction) => {
-    if (action.type === HYDRATE) {
-      const nextState = {
-        ...state, // use previous state
-        ...action.payload, // apply delta from hydration
-      }
-      return nextState
-    } else {
-      return reducers(state, action)
-    }
-  }*/
-
-// const x = { [$CombinedState]: undefined };
-
-/* const TEST = 
-    (state: reducersState, action: AnyAction) : Reducer<reducersState, AnyAction> => 
-    action.type === HYDRATE ? { ...x, ...state, ...action.payload } : reducers(state, action); */
-
 export const makeStore: MakeStore<reducersState> = (context: Context) => createStore(reducers, applyMiddleware(thunk));
 export const wrapper = createWrapper<reducersState>(makeStore, { debug: true });
