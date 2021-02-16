cd405dd2f68bcc20295e3f00180d245866d11edc
Evgenii Zaikin <evgenii.zaikin2607@gmail.com>
2021-02-16 22:46:22 +0300

    update create-page-wrapper component.

--- src/components/containers/create-page-wrapper/index.tsx
+++ src/components/containers/create-page-wrapper/index.tsx
@@ -2,7 +2,8 @@ import React, { FC, ReactElement } from 'react';
 import Containers from '@containers';
 import { 
     layout__container, layout__wrap, layout__save, 
-    layout__values, layout__divider, layout__top
+    layout__values, layout__divider, layout__top,
+    layout__spacer,
 } from './styles.scss';
 import { connect } from 'react-redux';
 import { reducersState } from '@store';
@@ -11,6 +12,8 @@ interface IProps {
     type: string,
 };
 
+const Spacer = () => <div className={ layout__spacer }></div>;
+
 const createPageWrapper: FC<IProps> = ({ type }) : ReactElement => {
     return (
         <div className={ layout__container }>
@@ -18,11 +21,11 @@ const createPageWrapper: FC<IProps> = ({ type }) : ReactElement => {
                 <div className={ layout__top }>
                     <Containers.TypeRadioButtons />
                     <hr className={ layout__divider } />
-                    { type === `word` && <Containers.CategorySelect /> }
+                    { type === `word` ? <Containers.CategorySelect /> : <Spacer /> }
                 </div>
                 <div className={ layout__values }>
                     <Containers.RuValueInput />
-                    { type === `word` && <Containers.EnValueInput /> }
+                    { type === `word` ? <Containers.EnValueInput /> : <Spacer /> }
                 </div>
                 <div className={ layout__save }>
                     <Containers.SaveButton />
--- src/components/containers/create-page-wrapper/styles.scss.d.ts
+++ src/components/containers/create-page-wrapper/styles.scss.d.ts
@@ -2,6 +2,7 @@ declare const styles: {
   readonly "layout__container": string;
   readonly "layout__divider": string;
   readonly "layout__save": string;
+  readonly "layout__spacer": string;
   readonly "layout__top": string;
   readonly "layout__values": string;
   readonly "layout__wrap": string;
