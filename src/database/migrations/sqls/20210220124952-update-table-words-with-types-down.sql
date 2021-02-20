cd43261d45c14d0ac7f1e1e0168ebc32f3260f9c
Evgenii Zaikin <evgenii.zaikin2607@gmail.com>
2021-02-20 15:54:04 +0300

    create db-migration with update table words of words types

---
+++ src/database/migrations/sqls/20210220124952-update-table-words-with-types-down.sql
@@ -0,0 +1,4 @@
+ALTER TABLE words
+DROP COLUMN word_is_expression,
+DROP COLUMN word_is_slang,
+DROP COLUMN word_is_abuse;
---
+++ src/database/migrations/sqls/20210220124952-update-table-words-with-types-up.sql
@@ -0,0 +1,4 @@
+ALTER TABLE words
+ADD COLUMN word_is_expression BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN word_is_slang BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN word_is_abuse BOOLEAN NOT NULL DEFAULT false;
