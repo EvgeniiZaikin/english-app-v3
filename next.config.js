97472d71ea7b68416f5fec39010dafa7bb205a00
Evgenii Zaikin <evgenii.zaikin2607@gmail.com>
2021-04-02 01:04:47 +0300

    Remove unused comments and code inside next.config.js.

--- next.config.js
+++ next.config.js
@@ -6,22 +6,15 @@ const path = require('path');
 module.exports = withTypescript(
   withSass({
     cssModules: true,
-    sassOptions: {
-      // includePaths: ['./src/client/pages/global.scss'],
-    },
     webpack: (config, { dev }) => {
       if (!config.plugins) config.plugins = [];
 
       config.plugins.push(
         new TypedCssModulesPlugin({
           globPattern: 'src/components/**/**/*.scss',
-          // globPattern: 'src/client/assets/styles/*.scss',
         })
       );
 
-      /* config.resolve.alias['@creators'] = path.join(__dirname, 'src/client/store/creators');
-      config.resolve.alias['@creators/sync'] = path.join(__dirname, 'src/client/store/creators/sync');
-      config.resolve.alias['@creators/async'] = path.join(__dirname, 'src/client/store/creators/async'); */
       config.resolve.alias['@presentations'] = path.join(__dirname, 'src/components/presentations');
       config.resolve.alias['@containers'] = path.join(__dirname, 'src/components/containers');
       config.resolve.alias['@wrappers'] = path.join(__dirname, 'src/components/wrappers');
@@ -30,7 +23,6 @@ module.exports = withTypescript(
       config.resolve.alias['@reducers/*'] = path.join(__dirname, 'src/store/reducers/*');
       config.resolve.alias['@utils/*'] = path.join(__dirname, 'src/utils/*');
       config.resolve.alias['@defaultStyles'] = path.join(__dirname, 'src/assets/styles/default.scss');
-      /* config.resolve.alias['@actions'] = path.join(__dirname, 'src/client/store/actions'); */
 
       return config;
     },
